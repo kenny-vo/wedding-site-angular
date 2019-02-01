877f9fae74bf0a01cde32214409c969ce2482d4e
Kenny Vo <kvo@kvo-mbp.austin.q2technology.com>
2019-02-01 16:48:58 -0600

    added cors

--- server.js
+++ server.js
@@ -2,11 +2,26 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var mongodb = require("mongodb");
 var ObjectID = mongodb.ObjectID;
+var cors = require('cors');
+
+app.use(cors());
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 var CONTACTS_COLLECTION = "contacts";
 
 var app = express();
 app.use(bodyParser.json());
+app.use(express.static('public'));
+
 
 // Create link to Angular build directory
 var distDir = __dirname + "/dist/";
