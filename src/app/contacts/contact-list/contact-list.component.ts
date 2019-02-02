96c29727ebe1923c1604a65a4734c20fd6a74bdd
Kenny Vo <kvo@kvo-mbp.local>
2019-02-01 18:40:05 -0600

    added consoles

--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -74,9 +74,12 @@ export class ContactListComponent implements OnInit {
 
   updateContact = (contact: Contact) => {
     var idx = this.getIndexOfContact(contact._id);
+    console.log(idx);
+    console.log(contact);
     if (idx !== -1) {
       this.contacts[idx] = contact;
       this.selectContact(contact);
+      console.log(contact)
     }
     return this.contacts;
   }
