d4c5508a8bdcbaca2f6dfe0996dce50f2a2bf479
Kenny Vo <kvo@kvo-mbp.austin.q2technology.com>
2019-02-04 11:54:34 -0600

    updated server

--- server.js
+++ server.js
@@ -33,6 +33,14 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:2701
   });
 });
 
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+}
+
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
 // CONTACTS API ROUTES BELOW
 
 // Generic error handler used by all endpoints.
