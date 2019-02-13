62ba2718133335efb828cadd0ee984cb6cb79c32
Kenny Vo <kvo@kvo-mbp.austin.q2technology.com>
2019-02-13 13:36:59 -0600

    updated comments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import { ContactDetailsComponent } from './contacts/contact-details/contact-deta
 import { ContactListComponent } from './contacts/contact-list/contact-list.component';
 import { PhotosComponent } from './photos/photos.component';
 
-import { GrdFilterPipe } from './contacts/grd-filter.pipe';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
 import { KlComponent } from './kl/kl.component';
@@ -19,6 +18,8 @@ import { HeaderComponent } from './header/header.component';
 import { FaqComponent } from './faq/faq.component';
 import { FooterComponent } from './footer/footer.component';
 import { PhotosKlComponent } from './photos-kl/photos-kl.component';
+
+import { GrdFilterPipe } from './contacts/grd-filter.pipe';
 import { hasProperty } from './contacts/property-filter.pipe';
 import { KlFilterPipe } from './contacts/kl-filter.pipe';
 import { UsFilterPipe } from './contacts/us-filter.pipe';
