fd0c2f7e15138a5a7982692ac123befe28b12b4e
Kenny Vo <kennyvo91@gmail.com>
2019-08-12 21:44:48 -0500

    spacing

--- src/app/gifts/gifts.component.ts
+++ src/app/gifts/gifts.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, Inject, OnInit } from '@angular/core';
+import { DOCUMENT } from "@angular/platform-browser";
 
 @Component({
   selector: 'app-gifts',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GiftsComponent implements OnInit {
 
-  constructor() { }
+  constructor(@Inject(DOCUMENT) private document: Document) { }
 
   ngOnInit() {
+    window.scroll(0,0);
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      document.getElementById('nav').classList.add('scrolled');
+    } else {
+      document.getElementById('nav').classList.remove('scrolled');
+    }
   }
 
 }
